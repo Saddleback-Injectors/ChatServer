9a783c59e5d032db145eb7e11f3809cfc9c13c0d
Tucker Harvey <coopert.harvey@gmail.com>
2020-03-10 15:58:59 -0700

    Chat server init

--- src/edu/saddleback/cs4b/Backend/ChatServer.java
+++ src/edu/saddleback/cs4b/Backend/ChatServer.java
@@ -1,4 +1,17 @@
 package edu.saddleback.cs4b.Backend;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
 public class ChatServer {
+    private static List<String> channels = new ArrayList<>();
+    private static BlockingQueue<ClientConnection> clients = new LinkedBlockingQueue<>();
+    private static int numClients = 0;
+
+    public static void main(String[] args) {
+        // will temporarily run the server, later done through
+        // our Main class
+    }
 }
