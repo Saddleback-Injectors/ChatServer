5ca4efe3b4b24eb77c705afa467c366b83be2bb2
Tucker Harvey <coopert.harvey@gmail.com>
2020-03-10 15:27:31 -0700

    added getter and setters

--- src/edu/saddleback/cs4b/Backend/Packet.java
+++ src/edu/saddleback/cs4b/Backend/Packet.java
@@ -5,4 +5,20 @@ import java.io.Serializable;
 public class Packet {
     private String type;
     private Serializable data;
+
+    public Packet(String type, Serializable data) {
+        setType(type);
+        setData(data);
+    }
+
+    private void setType(String type) {
+        this.type = type;
+    }
+
+    private void setData(Serializable data) {
+        this.data = data;
+    }
+
+    public String getType() { return type; }
+    public Serializable getData() { return data; }
 }
