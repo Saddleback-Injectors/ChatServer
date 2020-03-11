9d2bf7a6cdcf0293727288f80f5613c6016d836a
Tucker Harvey <coopert.harvey@gmail.com>
2020-03-10 22:51:17 -0700

    init ClientConnection and set up ctor

--- src/edu/saddleback/cs4b/Backend/ClientConnection.java
+++ src/edu/saddleback/cs4b/Backend/ClientConnection.java
@@ -1,4 +1,20 @@
 package edu.saddleback.cs4b.Backend;
 
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * This class will wrap necessary information about a client
+ * necessary to a connection
+ */
 public class ClientConnection {
+    private Socket socket;
+    private List<String> channels;
+    // maybe a publisher here later
+
+    public ClientConnection(Socket socket) {
+        this.socket = socket;
+        this.channels = new ArrayList<>();
+    }
 }
