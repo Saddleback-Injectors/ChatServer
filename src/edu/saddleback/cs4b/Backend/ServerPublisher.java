774fb304a6c42b09fd8e7482300c1556517ebd79
Tucker Harvey <coopert.harvey@gmail.com>
2020-03-14 11:15:55 -0700

    made serverPub a runnable

--- src/edu/saddleback/cs4b/Backend/ServerPublisher.java
+++ src/edu/saddleback/cs4b/Backend/ServerPublisher.java
@@ -2,9 +2,14 @@ package edu.saddleback.cs4b.Backend;
 
 import java.util.List;
 
-public class ServerPublisher {
+public class ServerPublisher implements Runnable {
     private List<ClientConnection> clients;
     ServerPublisher(List<ClientConnection> clients) {
         this.clients = clients;
+    }
+
+    @Override
+    public void run() {
+
     }
 }
