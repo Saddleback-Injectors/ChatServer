eabc9aa23a5d1fa5fc2c60d513942802e5cfa254
Tucker Harvey <coopert.harvey@gmail.com>
2020-03-14 12:17:35 -0700

    server pub has method to distribute messages

--- src/edu/saddleback/cs4b/Backend/ServerPublisher.java
+++ src/edu/saddleback/cs4b/Backend/ServerPublisher.java
@@ -1,19 +1,43 @@
 package edu.saddleback.cs4b.Backend;
 
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.util.List;
 import java.util.concurrent.BlockingQueue;
 
 public class ServerPublisher implements Runnable {
     private List<ClientConnection> clients;
     private BlockingQueue<Packet> messages;
+    private boolean isRunning;
+
     ServerPublisher(List<ClientConnection> clients,
                     BlockingQueue<Packet> messages) {
         this.clients = clients;
         this.messages = messages;
+        this.isRunning = true;
     }
 
     @Override
     public void run() {
+        while (isRunning) {
+            try {
+                Packet curPacket = messages.take();
+                distribute(curPacket);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
+    private void distribute(Packet packet) {
+        ObjectOutputStream out = null;
+        for (ClientConnection c : clients) {
+            out = c.getOutputStream();
+            try {
+                out.writeObject(packet);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
