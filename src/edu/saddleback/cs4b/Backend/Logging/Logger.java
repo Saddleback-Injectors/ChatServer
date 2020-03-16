a9627e17bf59a083a2059a06529b22af98397832
Tucker Harvey <coopert.harvey@gmail.com>
2020-03-15 22:31:08 -0700

    Logger: initial

---
+++ src/edu/saddleback/cs4b/Backend/Logging/Logger.java
@@ -0,0 +1,19 @@
+package edu.saddleback.cs4b.Backend.Logging;
+
+public class Logger implements LogSubject {
+
+    @Override
+    public void registerObserver(LogObserver o) {
+
+    }
+
+    @Override
+    public void removeObserver(LogObserver o) {
+
+    }
+
+    @Override
+    public void notifyObservers() {
+
+    }
+}
