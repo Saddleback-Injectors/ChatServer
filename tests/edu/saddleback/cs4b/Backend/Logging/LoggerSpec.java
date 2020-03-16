be1831a6b4d6b10a55178e9ecd63a5c351f8d787
Tucker Harvey <coopert.harvey@gmail.com>
2020-03-16 10:19:19 -0700

    LgrTst: observers notified when log is called

--- tests/edu/saddleback/cs4b/Backend/Logging/LoggerSpec.java
+++ tests/edu/saddleback/cs4b/Backend/Logging/LoggerSpec.java
@@ -4,10 +4,15 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class LoggerSpec {
     private Logger logger;
+    private final PrintStream systemOut = System.out;
+    private final ByteArrayOutputStream consoleOut = new ByteArrayOutputStream();
 
     @BeforeEach
     void init() {
@@ -31,5 +36,31 @@ class LoggerSpec {
     void logHasOneObserverWhenObserverSubscribes() {
         LogToConsole observer = new LogToConsole(logger);
         assertEquals(1, logger.getObservers().size());
+    }
+
+    @Test
+    @DisplayName("Logger has message when called to log")
+    void loggerHasMessageWhenCallToLog() {
+        logger.log("test");
+        assertEquals("test", logger.getMessage());
+    }
+
+    @Test
+    @DisplayName("Observers notified when log is called")
+    void observersNotifiedOnCallToLog() {
+        changePrintStream();
+        LogToConsole observer = new LogToConsole(logger);
+        logger.log("test");
+        String str = consoleOut.toString();
+        assertEquals("test\n", str);
+        restorePrintStream();
+    }
+
+    private void changePrintStream() {
+        System.setOut(new PrintStream(consoleOut));
+    }
+
+    private void restorePrintStream() {
+        System.setOut(systemOut);
     }
 }
