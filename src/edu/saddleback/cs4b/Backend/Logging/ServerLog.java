ce2f364ee5610b8ab49e7a70acabb4c5a164c09a
Tucker Harvey <coopert.harvey@gmail.com>
2020-03-15 23:50:25 -0700

    renamed chatserverlogger -> serverlogger

--- src/edu/saddleback/cs4b/Backend/Logging/ServerLog.java
+++ src/edu/saddleback/cs4b/Backend/Logging/ServerLog.java
@@ -1,6 +1,6 @@
 package edu.saddleback.cs4b.Backend.Logging;
 
-public class ChatServerLog {
+public class ServerLog {
     private static final Logger logger = new Logger();
 
     public synchronized static void log(String msg) {
