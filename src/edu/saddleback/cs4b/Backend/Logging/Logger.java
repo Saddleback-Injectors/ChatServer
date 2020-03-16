c9a6a8bc8d6c6acc27b5a96f19e9beaa8ab3da53
Tucker Harvey <coopert.harvey@gmail.com>
2020-03-15 22:47:06 -0700

    Logger: ctor added and def

--- src/edu/saddleback/cs4b/Backend/Logging/Logger.java
+++ src/edu/saddleback/cs4b/Backend/Logging/Logger.java
@@ -1,6 +1,16 @@
 package edu.saddleback.cs4b.Backend.Logging;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Logger implements LogSubject {
+    private List<String> errorMessages;
+    private List<LogObserver> observers;
+
+    public Logger() {
+        this.errorMessages = new ArrayList<>();
+        this.observers = new ArrayList<>();
+    }
 
     @Override
     public void registerObserver(LogObserver o) {
