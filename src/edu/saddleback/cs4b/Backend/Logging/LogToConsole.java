0ef1d471c328eb51b67c1580e31bc94f0905af9b
Tucker Harvey <coopert.harvey@gmail.com>
2020-03-15 23:03:28 -0700

    LgCons: init

---
+++ src/edu/saddleback/cs4b/Backend/Logging/LogToConsole.java
@@ -0,0 +1,13 @@
+package edu.saddleback.cs4b.Backend.Logging;
+
+public class LogToConsole implements LogObserver, LogDisplayable {
+    @Override
+    public void display() {
+
+    }
+
+    @Override
+    public void update(String error) {
+
+    }
+}
