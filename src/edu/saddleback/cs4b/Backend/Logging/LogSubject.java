bee9375ae713c98013c70ffbd38e3c0b598726da
Tucker Harvey <coopert.harvey@gmail.com>
2020-03-15 22:29:17 -0700

    LogObs: abstract methods added

--- src/edu/saddleback/cs4b/Backend/Logging/LogSubject.java
+++ src/edu/saddleback/cs4b/Backend/Logging/LogSubject.java
@@ -1,4 +1,7 @@
 package edu.saddleback.cs4b.Backend.Logging;
 
 public interface LogSubject {
+    void registerObserver(LogObserver o);
+    void removeObserver(LogObserver o);
+    void notifyObservers();
 }
