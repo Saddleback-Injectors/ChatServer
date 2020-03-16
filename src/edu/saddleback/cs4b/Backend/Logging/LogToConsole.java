b005f91edffdfdecefee9f8d946eaa3bb7ae54f2
Tucker Harvey <coopert.harvey@gmail.com>
2020-03-15 23:05:22 -0700

    LgCons: ctor def

--- src/edu/saddleback/cs4b/Backend/Logging/LogToConsole.java
+++ src/edu/saddleback/cs4b/Backend/Logging/LogToConsole.java
@@ -1,6 +1,15 @@
 package edu.saddleback.cs4b.Backend.Logging;
 
 public class LogToConsole implements LogObserver, LogDisplayable {
+    private LogSubject logger;
+    private String logMessage;
+
+    public LogToConsole(LogSubject subject) {
+        this.logger = subject;
+        logger.registerObserver(this);
+        this.logMessage = "";
+    }
+
     @Override
     public void display() {
 
