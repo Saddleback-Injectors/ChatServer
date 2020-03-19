4ea35f5065c0f5b6be8ce548e51336b05e19294b
Tucker Harvey <coopert.harvey@gmail.com>
2020-03-19 14:09:57 -0700

    ui display fields not editable

--- src/edu/saddleback/cs4b/UI/ServerScreenController.java
+++ src/edu/saddleback/cs4b/UI/ServerScreenController.java
@@ -3,9 +3,14 @@ package edu.saddleback.cs4b.UI;
 import edu.saddleback.cs4b.Backend.Logging.LogObserver;
 import edu.saddleback.cs4b.Backend.Logging.LogSubject;
 import edu.saddleback.cs4b.Backend.Logging.ServerLog;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
 
 public class ServerScreenController implements LogObserver {
     private LogSubject logger = ServerLog.getLogger();
+
+    @FXML
+    private TextArea logScreen;
 
     ServerScreenController() {
         logger.registerObserver(this);
