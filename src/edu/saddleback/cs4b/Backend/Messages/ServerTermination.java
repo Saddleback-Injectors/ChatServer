3c8da221b5a1b51f1b476943a54f2d09c05b0809
Tucker Harvey <coopert.harvey@gmail.com>
2020-03-22 22:31:15 -0700

    created a server termination message type

---
+++ src/edu/saddleback/cs4b/Backend/Messages/ServerTermination.java
@@ -0,0 +1,7 @@
+package edu.saddleback.cs4b.Backend.Messages;
+
+public class ServerTermination extends BaseMessage {
+    public ServerTermination() {
+        super("Server", "Termination");
+    }
+}
