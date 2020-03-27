be9f1a692afc9e420a95ecb9bcf676952d5006b6
Tucker Harvey <coopert.harvey@gmail.com>
2020-03-27 13:47:23 -0700

    request msg ctors

--- src/edu/saddleback/cs4b/Backend/Messages/RequestMessage.java
+++ src/edu/saddleback/cs4b/Backend/Messages/RequestMessage.java
@@ -1,4 +1,28 @@
 package edu.saddleback.cs4b.Backend.Messages;
 
-public class RequestMessage {
+import edu.saddleback.cs4b.Backend.History;
+
+public class RequestMessage extends BaseMessage {
+    private RequestType type;
+    private String channel;
+    private History channelHistory;
+
+    private RequestMessage(String sender, String type,
+                           RequestType requestType,
+                           String channel, History channelHistory) {
+        super(sender, type);
+        this.type = requestType;
+        this.channel = channel;
+        this.channelHistory = channelHistory;
+    }
+
+    public RequestMessage(RequestType requestType, String channel) {
+        this("", requestType.getType(), requestType, channel, null);
+    }
+
+    public RequestMessage(String sender, RequestType requestType, String channel) {
+        this(sender, requestType.getType(), requestType, channel, null);
+    }
+
+    
 }
