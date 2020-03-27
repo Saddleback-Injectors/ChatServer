53d91819bc6543268439694bed8ac9d610577b7d
Tucker Harvey <coopert.harvey@gmail.com>
2020-03-27 10:11:47 -0700

    ctor defined

--- src/edu/saddleback/cs4b/Backend/History.java
+++ src/edu/saddleback/cs4b/Backend/History.java
@@ -1,7 +1,14 @@
 package edu.saddleback.cs4b.Backend;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 
 public class History implements Serializable {
-    
+    private ArrayList<String> msgLog;
+    private byte[] recentPic;
+
+    History() {
+        this.msgLog = new ArrayList<>();
+        this.recentPic = null;
+    }
 }
